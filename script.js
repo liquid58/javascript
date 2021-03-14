4d4a01852f6ef50a0916d948ec283e0a785406f5
liquid58 <39938685+liquid58@users.noreply.github.com>
2021-03-14 15:03:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,31 +25,28 @@ function promptfunction(){
 
 
 function lastfun(){
-    alert("total marks is:- 500, click okay for next.");
-    var no1=500;
-   var no2=prompt("give your obtain marks for check grade.");
-    if(no2*100/no1==90%){
-        alert("your grade is A+");
-    }
-    else if(no2*100/no1==80){
-    alert("your grade is A");
-    }
-    else if(no2*100/no1==65){
-    alert("your grade is B+");
-    }
-    else if(no2*100/no1==55){
-    alert("your grade is B");
-    }
-    else if(no2*100/no1==45){
-    alert("your grade is C");
-    }
-    else if(no2*100/no1==33){
-    alert("your grade is D");
-    }
-    else{
-    alert("you are fail");
-    }
-}
-
+    var a=prompt("percentage")
+   if(a>85)
+    {//if percent > 85 --A
+      document.write('Grade - A');}
+else if(a<=85 && a>80)
+    {//80<percent80=85  --A
+      document.write('Grade - A-');}
+else if(a>70 && a<=80)
+    {//70<percent<=80 --B
+      document.write('Grade - B');}
+else if(a>60 && a<=70)
+    {//60<percent<=70  --C
+      document.write('Grade - C');}
+else if(a>40 && a<=60)
+    {//40<percent<=60  --D
+      document.write('Grade - D');}
+else if(a<=40)
+    {//percent<=40  --E
+      if(a<=35)
+        {//failed condition
+          document.write('Grade - E Candidate failed');}
+      else
+        {document.write('Grade - E');}}
 
 
